c3d38f3482ec5bae521702c6d5b19a6842719325
IanSowell <88625523+IanSowell@users.noreply.github.com>
2023-09-08 13:51:07 -0500

    Initial File Indexing

---
+++ home.js
@@ -0,0 +1,10 @@
+function fadeBanner(banner){
+    banner.style.opacity = 0.5;
+}
+function unfadeBanner(banner){
+    banner.style.opacity = 1;
+}
+function bigImg(x) {
+    x.style.opacity=.5;
+  }
+  
